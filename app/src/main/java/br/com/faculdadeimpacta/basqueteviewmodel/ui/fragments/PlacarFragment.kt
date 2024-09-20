8e5a819e4c9b5f2a1c28bdd3d9558be2501ddace
Fernando Tanaka <fernando.tanaka@outlook.com>
2024-09-20 19:54:36 -0300

    logica implamentada no fragmento

--- app/src/main/java/br/com/faculdadeimpacta/basqueteviewmodel/ui/fragments/PlacarFragment.kt
+++ app/src/main/java/br/com/faculdadeimpacta/basqueteviewmodel/ui/fragments/PlacarFragment.kt
@@ -6,17 +6,50 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import br.com.faculdadeimpacta.basqueteviewmodel.R
+import br.com.faculdadeimpacta.basqueteviewmodel.databinding.FragmentPlacarBinding
 
 
 class PlacarFragment : Fragment() {
 
+    private var _binding: FragmentPlacarBinding? = null
+    private val binding get() = _binding!!
+    private var placarTimeCasa = 0
+    private var placarTimeVisitante = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_placar, container, false)
+        _binding = FragmentPlacarBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.buttonCasa3Pontos.setOnClickListener { adicionarPontosTimeCasa(3) }
+        binding.buttonCasa2Pontos.setOnClickListener { adicionarPontosTimeCasa(2) }
+        binding.buttonCasa1Ponto.setOnClickListener { adicionarPontosTimeCasa(1) }
+
+        binding.buttonVisitante3Pontos.setOnClickListener { adicionarPontosTimeVisitante(3) }
+        binding.buttonVisitante2Pontos.setOnClickListener { adicionarPontosTimeVisitante(2) }
+        binding.buttonVisitante1Ponto.setOnClickListener { adicionarPontosTimeVisitante(1) }
+
+        binding.buttonReset.setOnClickListener {
+            adicionarPontosTimeCasa(-placarTimeCasa)
+            adicionarPontosTimeVisitante(-placarTimeVisitante)
+        }
+    }
+
+    fun adicionarPontosTimeCasa(quantidadePontos: Int) {
+        placarTimeCasa += quantidadePontos
+        binding.textViewPlacarCasa.text = placarTimeCasa.toString()
+    }
+
+    fun adicionarPontosTimeVisitante(quantidadePontos: Int) {
+        placarTimeVisitante += quantidadePontos
+        binding.textViewPlacarVisitante.text = placarTimeVisitante.toString()
     }
 
 }
